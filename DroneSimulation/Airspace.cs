04c8193ed548e4be9e8429f91a0c9a5f1770d0e1
Christopher Lind <Christopher.lind.c@gmail.com>
2021-01-18 18:59:08 +0100

    Started The Drone Simulation Basic Drone Class and basic methods (No implementation) Basic Airspace Class and some implementet methods

---
+++ DroneSimulation/Airspace.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DroneSimulation
+{
+    class Airspace
+    {
+        List<Drone> _drones = new List<Drone>();
+        List<Drone[]> _collidingDrones = new List<Drone[]>();
+
+
+        public void FlyDrones()
+        {
+            foreach(Drone drone in _drones)
+            {
+                drone.Fly();
+            }
+        }
+
+        public float DroneDist(Drone x, Drone y)
+        {
+            float xRelPos = x.Position[0] - y.Position[0];
+            float yRelPos = y.Position[1] - y.Position[1];
+            float sqrtDistance = (float)(Math.Pow(xRelPos, 2) + Math.Pow(yRelPos, 2));
+            return (float)Math.Sqrt(sqrtDistance);
+        }
+
+        public void AddDrone(Drone newDrone)
+        {
+            _drones.Add(newDrone);
+        }
+
+
+        // TODO: finish this method, should probably be in several methods.
+        public void WillCollide()
+        {
+            List<Drone[]> collidingDronePairs = new List<Drone[]>();
+            List<Drone> collidingDrones = new List<Drone>();
+            Drone xDrone;
+            Drone yDrone;
+
+            for (int i = 0; i < _drones.Count - 1; i++)
+            {
+                for (int j = i + 1; j < _drones.Count; j++)
+                {
+                    xDrone = _drones[i];
+                    yDrone = _drones[j];
+                    float distance = DroneDist(xDrone, yDrone);
+                    if (distance < 5f)
+                    {
+                        if (!collidingDrones.Contains(xDrone) 
+                            && !collidingDrones.Contains(xDrone))
+                        {
+                            collidingDronePairs.Add(new Drone[] { xDrone, yDrone });
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ DroneSimulation/Drone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DroneSimulation
+{
+    class Drone
+    {
+        float _speed;
+        float[] _destination;
+
+        public float[] Position { get; private set; }
+
+        public Drone(float[] position, float speed, float[] destination)
+        {
+            Position = position;
+            _speed = speed;
+            _destination = destination;
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
